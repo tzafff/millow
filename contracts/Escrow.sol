3ba10a094b5242d35cddc967b6c929e05a6d9130
tzafff <christzaf99@gmail.com>
2024-02-22 21:16:25 +0200

    List Property /w tests

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -2,11 +2,7 @@
 pragma solidity ^0.8.0;
 
 interface IERC721 {
-    function transferFrom(
-        address _from,
-        address _to,
-        uint256 _id
-    ) external;
+    function transferFrom(address _from, address _to, uint256 _id) external;
 }
 
 contract Escrow {
@@ -15,11 +11,40 @@ contract Escrow {
     address public lender;
     address public inspector;
 
-    constructor(address _nftAddress, address payable _seller, address _inspector, address _lender) {
+    modifier onlySeller() {
+        require(msg.sender == seller, "Only seller can call this method");
+        _;
+    }
+
+    mapping(uint256 => bool) public isListed;
+    mapping(uint256 => uint256) public purchasePrice;
+    mapping(uint256 => uint256) public escrowAmount;
+    mapping(uint256 => address) public buyer;
+
+    constructor(
+        address _nftAddress,
+        address payable _seller,
+        address _inspector,
+        address _lender
+    ) {
         nftAddress = _nftAddress;
         seller = _seller;
         lender = _lender;
         inspector = _inspector;
     }
 
+    function list(
+        uint256 _nftID,
+        address _buyer,
+        uint256 _purchasePrice,
+        uint256 _escrowAmount
+    ) public  payable onlySeller {
+        // Transfer NFT from seller to this contract
+        IERC721(nftAddress).transferFrom(msg.sender, address(this), _nftID);
+
+        isListed[_nftID] = true;
+        purchasePrice[_nftID] = _purchasePrice;
+        escrowAmount[_nftID] = _escrowAmount;
+        buyer[_nftID] = _buyer;
+    }
 }
