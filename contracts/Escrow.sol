cd42ee7e399ac0ee40715a77acebb72adb582e14
tzafff <christzaf99@gmail.com>
2024-02-22 20:44:10 +0200

    Deploy Escrow,Nft Contract /w tests

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -10,5 +10,16 @@ interface IERC721 {
 }
 
 contract Escrow {
+    address public nftAddress;
+    address payable public seller;
+    address public lender;
+    address public inspector;
+
+    constructor(address _nftAddress, address payable _seller, address _inspector, address _lender) {
+        nftAddress = _nftAddress;
+        seller = _seller;
+        lender = _lender;
+        inspector = _inspector;
+    }
 
 }
