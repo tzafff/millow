7e098b1215f328386aa090531bf40d3f4d31a430
tzafff <christzaf99@gmail.com>
2024-02-25 17:08:53 +0200

    List of Properties

--- src/App.js
+++ src/App.js
@@ -16,14 +16,52 @@ import config from "./config.json";
 function App() {
   const [account, setAccount] = useState(null);
 
+  const [provider, setProvider] = useState(null);
+
+  const [escrow, setEscrow] = useState(null);
+
+  const [homes, setHomes] = useState([]);
+
   const loadBlockchainData = async () => {
     const provider = new ethers.providers.Web3Provider(window.ethereum);
+    setProvider(provider);
 
-    window.ethereum.on('accountsChanged', async () => {
-      const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
-      const account = ethers.utils.getAddress(accounts[0])
-      setAccount(account)
-    })
+    const network = await provider.getNetwork();
+
+    // Get JS version of NFT contract
+    const realEstate = new ethers.Contract(
+      config[network.chainId].realEstate.address,
+      RealEstate,
+      provider
+    );
+    const totalSupply = await realEstate.totalSupply();
+    const homes = [];
+
+    for (var i = 1; i <= totalSupply; i++) {
+      const uri = await realEstate.tokenURI(i);
+      const response = await fetch(uri);
+      const metadata = await response.json();
+      homes.push(metadata);
+    }
+    setHomes(homes);
+
+    console.log(homes);
+
+    // Get JS version of escrow contract
+    const escrow = new ethers.Contract(
+      config[network.chainId].escrow.address,
+      Escrow,
+      provider
+    );
+    setEscrow(escrow);
+
+    window.ethereum.on("accountsChanged", async () => {
+      const accounts = await window.ethereum.request({
+        method: "eth_requestAccounts",
+      });
+      const account = ethers.utils.getAddress(accounts[0]);
+      setAccount(account);
+    });
   };
 
   useEffect(() => {
@@ -32,9 +70,32 @@ function App() {
 
   return (
     <div>
-      <Navigation account={account} setAccount={setAccount}/>
+      <Navigation account={account} setAccount={setAccount} />
+      <Search />
       <div className="cards__section">
-        <h3>Welcome to Millow</h3>
+        <h3>Homes For You</h3>
+
+        <hr />
+
+        <div className="cards">
+          {homes.map((home, index) => (
+
+            <div className="card" key={index}>
+              <div className="card__image">
+                <img src={home.image} alt="Home" />
+              </div>
+              <div className="card__info">
+                <h4>{home.attributes[0].value}</h4>
+                <p>
+                  <strong>{home.attributes[2].value}</strong> beds |
+                  <strong>{home.attributes[2].value}</strong> bathroom |
+                  <strong>{home.attributes[2].value}</strong> sqft
+                </p>
+                <p>{home.address}</p>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
